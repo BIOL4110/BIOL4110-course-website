76932fa9ac470e7c2e201b4d0adbbbc774e6240d
Joey Bernhardt <joey.bernhardt@gmail.com>
2024-10-24 12:19:09 -0400

    updating class 9 notes

--- content/inclass-9.R
+++ content/inclass-9.R
@@ -42,3 +42,42 @@ predict(my_lm, newdata = tibble(wt = 3:5)) %>%
 
 
 coef(my_lm)
+
+
+names(my_lm)
+
+
+summary(my_lm)
+names(summary(my_lm))
+
+
+summary(my_lm)$adj.r.squared
+
+
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+
+
+gapminder_Zimbabwe <- gapminder %>% 
+  filter(country == "Zimbabwe")
+
+gapminder_Zimbabwe %>% 
+  ggplot(aes(x = year, y = lifeExp)) + 
+  geom_point()
+
+
+ggplot(gapminder_Zimbabwe, aes(year,lifeExp)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+
+ggplot(gapminder_Zimbabwe, aes(year,lifeExp)) +
+  geom_point() +
+  geom_smooth(method = "lm",
+              formula = y ~ poly(x,2), 
+              se = F)
+
+
+?poly
