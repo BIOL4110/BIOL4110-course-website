fd29f8642815d3d1c8ac610232c518153a2adb57
Joey Bernhardt <joey.bernhardt@gmail.com>
2024-10-29 08:57:51 -0400

    adding notes from class 10

--- content/inclass-9.R
+++ content/inclass-9.R
@@ -8,25 +8,64 @@ library(gapminder)
 library(broom)
 
 
+my_lm <- lm(mpg ~ wt, data = mtcars)
+class(my_lm)
+str(my_lm)
+
+my_lm
+summary(my_lm)
+
+
 
 (my_lm <- lm(mpg ~ wt, data = mtcars))
 my_lm
+
 
 
 tidy(my_lm)
 
 
 
+tidy(my_lm)
+
+
 augment(my_lm) %>% 
   print(n = 5)
 
 
+
+augment(my_lm) %>% 
+  print(n = 5)
+
+augment(my_lm, newdata = tibble(wt = 3:5)) 
 ?augment()
 
-augment(my_lm, newdata = tibble(wt = 3:5))
+df2 <- augment(my_lm, newdata = tibble(wt = 1:6), se_fit = TRUE)
 
 
+df2 %>% 
+  ggplot(aes(x = wt, y = .fitted)) + geom_line() +
+  geom_point(aes(x = wt, y = mpg), data = mtcars, color = "red")
+
+
+
+mtcars %>% 
+  ggplot(aes(x = wt, y = mpg)) + geom_point() +
+  geom_smooth(method = "lm", color = "orange") +
+  geom_line(aes(x = wt, y = .fitted), data = df2, color = "pink")
+
+
+
+
+names(my_lm)
+my_lm$coefficients
+
+
+
+?geom_smooth()
 glance(my_lm)
+
+?augment
 
 
 
